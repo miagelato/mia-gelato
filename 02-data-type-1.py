a1190eb151ebc6a8ef72cce465cbbcbad5c834c7
Mia.L <MiaL@MiaLui-MacBook-Pro.local>
2018-05-13 22:00:32 +0900

    add 02-data-type-1.py > -learn  int, float, str

---
+++ 02-data-type-1.py
@@ -0,0 +1,26 @@
+# 정수 int - integer
+print(1 + 1)
+# 소수 float
+print(1.5+2.1)
+# 문자 str - string
+print ("hello")
+
+#문자열 더하기
+print("hello" + "1")
+
+#정수 소수 간 변경
+print(4//2)
+print(int(4/2))
+print(float(2))
+
+#문자열 숫자 간 변경
+#문자
+print("1")
+#숫자
+print(int("1"))
+#위 1, 1은 같아보이지만 같지 않음
+print(int("1.5"))
+#1) 1
+#print(int(1.5))
+#2) error -> 정답!!
+# >> 문자열에 소수가 담겨있으면 제대로 변환하지 않는다
